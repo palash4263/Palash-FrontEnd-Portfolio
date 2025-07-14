f94e34d321da8cd9d02f1da7562bad78d264df74
Palash <palashmishra47@gmail.com>
2025-07-14 14:09:33 +0530

    Remove unused Cursor import to fix Netlify build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cursor from "react-awesome-cursor";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
